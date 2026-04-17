d15a46618fc17203b98655f92868c5267ad79c7b
Larsen Midayanes <techtacoder@gmail.com>
2026-04-17 21:22:29 +0100

    update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { usePreferences } from '../context/PreferencesContext';
-import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
   const navigate = useNavigate();
